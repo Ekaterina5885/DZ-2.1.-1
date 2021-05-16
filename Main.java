8a9b9b6fbab472f1de7230f2cbeda896c6cf487a
Ekaterina Kolesnikova <1005885@mail.ru>
2021-05-16 14:58:12 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 1_000;
+        int mile = 20;
+
+        int bonus = priceTicket / mile;
+
+        System.out.println("Начисленные бонусные мили: " + bonus);
+    }
+}
